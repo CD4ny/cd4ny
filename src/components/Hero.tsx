e1e249ac983efecc4793886f21d1c266ceb5b32b
Chavi <64395192+CD4ny@users.noreply.github.com>
2023-09-07 00:33:25 -0400

    if

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,18 +2,14 @@ import profile from "../assets/profile.png";
 
 function Hero() {
   const description =
-    "Estudiante de ingeieria de software ," +
-    "programador especializado en Backend," +
-    " con experiencia en tecnologias frontend," +
-    "interesado en diseño del sistema y grafico; "+
-    "entusiasta de la IA, la algoritmia, las matemáticas. ";
+    "Estudiante de ingeniería de software, programador especializado en Backend, con experiencia en tecnologías frontend, interesado en diseño del sistema y gráfico; entusiasta de la IA, la algoritmia y las matemáticas. ";
   const front_tech = " React, TailwindCss, JQuery, CSS, Bootstrap.";
   const back_tech = " Django, Flask, NextJs, SpringBoot, Express, WebFlux.";
   const db_tech = " MySql, PostgreSql, MongoDB, SqLite.";
 
   return (
     <section id="about" className="flex flex-col-reverse md:flex-row p-4 justify-center items-center min-h-screen pt-20 sm:pt-0">
-      <div className=" max-w-sm">
+      <div className=" max-w-sm ">
         <h1 className="text-lg text-orange_(web) font-nunito">
           Daniel Chaviano Perez
         </h1>
@@ -21,7 +17,7 @@ function Hero() {
 
         <p className="text-xs font-thin font-montserrat">{description}</p>
         <br />
-        <h1 className="text-blue_(munsell)-800 font-nunito">
+        <h1 className="text-md text-blue_(munsell)-800 font-nunito">
           Tecnologias Web
         </h1>
         <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">Backend:</p>
@@ -32,13 +28,10 @@ function Hero() {
         <p></p>
         <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">Database: </p>
         <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">{db_tech}</p>
-        <p></p>
-        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">DevOps: </p>
-        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-400">{"Pronto..."}</p>
       </div>
       <img
         src={profile}
-        className="bg-white-100 hover:sepia-0 duration-1000 hover:transition-all hover:duration-1000 transition-all sepia rounded-full w-40 md:w-64 animate-fade-left animate-once animate-duration-300 animate-delay-200 animate-ease-linear animate-normal animate-fill-both"
+        className="bg-white-100 mx-12 hover:sepia-0 duration-1000 hover:transition-all hover:duration-1000 transition-all sepia rounded-full w-40 md:w-64 animate-fade-left animate-once animate-duration-300 animate-delay-200 animate-ease-linear animate-normal animate-fill-both"
         alt="Logo"
       />
     </section>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function Hero() {
 
+  var user = "CD4ny";
+
+  const [projects , setprojects] = useState(0) 
+
   return (
-    <section id="projects" className="flex flex-col-reverse md:flex-row p-4 justify-center items-center">
-      Projects
+    <section id="projects" className="flex flex-col p-4 justify-center items-center min-h-screen">
+      <h1 className="text-md text-blue_(munsell)-800 font-nunito">Projects</h1>
+      <a href="#" className="flex flex-col border my-4 rounded-lg">
+            <div className="px-2 py-1">
+            <h3 className="text-md font-light font-nunito text-blue_(munsell)-700">Nomproj</h3>
+            <p className="text-xs font-extralight font-montserrat text-orange_(web)-800">"Global Game Jam register module"</p>
+            </div>
+            <img className="w-72" src="https://raw.githubusercontent.com/CD4ny/GGJ_REG/master/Screenshot.webp" alt="project-screenshot" />
+      </a>
     </section>
   );
 }
