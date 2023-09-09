66162c1757fc7a475f7dcacedeaa31471c1609a3
Chavi <64395192+CD4ny@users.noreply.github.com>
2023-09-09 09:16:08 -0400

    final

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,37 +1,66 @@
 import profile from "../assets/profile.png";
+import { faLinkedin , faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Hero() {
   const description =
     "Estudiante de ingeniería de software, programador especializado en Backend, con experiencia en tecnologías frontend, interesado en diseño del sistema y gráfico; entusiasta de la IA, la algoritmia y las matemáticas. ";
   const front_tech = " React, TailwindCss, JQuery, CSS, Bootstrap.";
-  const back_tech = " Django, Flask, NextJs, SpringBoot, Express, WebFlux.";
+  const back_tech = " Django, Flask, NextJs, SpringBoot, NestJs, WebFlux.";
   const db_tech = " MySql, PostgreSql, MongoDB, SqLite.";
 
   return (
-    <section id="about" className="flex flex-col-reverse md:flex-row p-4 justify-center items-center min-h-screen pt-20 sm:pt-0">
+    <section
+      id="about"
+      className="flex flex-col-reverse md:flex-row p-4 justify-center items-center min-h-screen  sm:pt-0"
+    >
       <div className=" max-w-sm ">
         <h1 className="text-lg text-orange_(web) font-nunito">
           Daniel Chaviano Perez
         </h1>
-        <h6 className="text-md text-blue_(munsell)-800 font-nunito">Software engineer</h6>
+        <a href="https://www.linkedin.com/in/chavianodaniel99">
+          <FontAwesomeIcon className="text-white-400" icon={faLinkedin} size="xl" />
+        </a>
+        <a href="https://github.com/CD4ny">
+          <FontAwesomeIcon className="text-white-400 px-3" size="xl" icon={faGithubSquare} />
+        </a>
+        <a href="mailto://chavianodaniel99@gmail.com">
+          <FontAwesomeIcon className="text-white-400" size="xl" icon={faEnvelopeSquare} />
+        </a>
+        <h6 className="text-md text-blue_(munsell)-800 font-nunito">
+          Software engineer
+        </h6>
 
         <p className="text-xs font-thin font-montserrat">{description}</p>
         <br />
         <h1 className="text-md text-blue_(munsell)-800 font-nunito">
           Tecnologias Web
         </h1>
-        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">Backend:</p>
-        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">{back_tech}</p>
+        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">
+          Backend:
+        </p>
+        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">
+          {back_tech}
+        </p>
         <br />
-        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">Frontend:</p>
-        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">{front_tech}</p>
+        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">
+          Frontend:
+        </p>
+        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">
+          {front_tech}
+        </p>
         <p></p>
-        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">Database: </p>
-        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">{db_tech}</p>
+        <p className="inline text-xs font-light font-nunito text-blue_(munsell)-700">
+          Database:{" "}
+        </p>
+        <p className="inline text-xs font-extralight font-montserrat text-orange_(web)-800">
+          {db_tech}
+        </p>
       </div>
       <img
         src={profile}
-        className="bg-white-100 mx-12 hover:sepia-0 duration-1000 hover:transition-all hover:duration-1000 transition-all sepia rounded-full w-40 md:w-64 animate-fade-left animate-once animate-duration-300 animate-delay-200 animate-ease-linear animate-normal animate-fill-both"
+        className="bg-white-100 mx-12 m-8 hover:sepia-0 duration-1000 hover:transition-all hover:duration-1000 transition-all sepia rounded-full w-40 md:w-64 animate-fade-left animate-once animate-duration-300 animate-delay-200 animate-ease-linear animate-normal animate-fill-both"
         alt="Logo"
       />
     </section>
